b62ae7d62a0f6e7e58c6231605d20d7a86d74db5
Alexander Robinson <alexander.robinson@awi.de>
2025-10-29 18:07:11 +0100

    Batchelor2019: updated mapping script.

--- Batchelor2019/batchelor_to_grid.sh
+++ Batchelor2019/batchelor_to_grid.sh
@@ -10,5 +10,9 @@ cdo griddes Batchelor2019_ice_masks.nc > grid_NH-lonlat-0.5deg.txt
 sed -i -e 's/generic/latlon/g' grid_NH-lonlat-0.5deg.txt
 rm grid_NH-lonlat-0.5deg.txt-e
 
+# Generate mapping weights
 cdo gencon,../maps/grid_${grid_name_tgt}.txt -setgrid,grid_${grid_name_src}.txt ${nc_src} scrip-con_${grid_name_src}_${grid_name_tgt}.nc
 
+# Perform remapping
+nc_out=${grid_name_tgt}_${nc_src}
+cdo remap,grid_${grid_name_tgt}.txt,scrip-con_${grid_name_src}_${grid_name_tgt}.nc ${nc_src} ${nc_out}
