4ef8dbd763d3e4928e3c5c775e2cef40eaee853c
Alexander Robinson <alexander.robinson@awi.de>
2025-10-29 18:05:12 +0100

    Batchelor2019: Added missing source file.

---
+++ Batchelor2019/batchelor_to_grid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+domain=North
+grid_name_src=NH-lonlat-0.5deg
+grid_name_tgt=NH-32KM
+nc_src=Batchelor2019_ice_masks.nc 
+
+# Generate source grid file, make sure to set grid type to latlon
+cdo griddes Batchelor2019_ice_masks.nc > grid_NH-lonlat-0.5deg.txt
+sed -i -e 's/generic/latlon/g' grid_NH-lonlat-0.5deg.txt
+rm grid_NH-lonlat-0.5deg.txt-e
+
+cdo gencon,../maps/grid_${grid_name_tgt}.txt -setgrid,grid_${grid_name_src}.txt ${nc_src} scrip-con_${grid_name_src}_${grid_name_tgt}.nc
+
